83ddc520a1fbad1e39da40c4a6cadf4298d7a4e1
Saleha <salehakhalid4@gmail.com>
2024-09-06 15:03:16 +1000

    Scrape rent history medians for suburbs

---
+++ scripts/scrape_rent_history.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Retrieve the data from the excel file (for all sheets)
+rent_history_excel = "https://www.dffh.vic.gov.au/moving-annual-rent-suburb-march-quarter-2024-excel"
+
+# Load all sheets into a dictionary of DataFrames
+rent_history_df_dict = pd.read_excel(rent_history_excel, sheet_name=None, engine='openpyxl')
+
+# Loop through each sheet and save to individual CSV files
+for sheet_name, df in rent_history_df_dict.items():
+    # Save each sheet to a separate CSV file
+    df.to_csv(f'../data/landing/rent_history_{sheet_name}.csv', index=False)
+
+print("Rental history data from all sheets has been downloaded!")
